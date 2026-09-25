3cd631438e7a970d26ce843126f4cdea9e784115
abdullah14120 <abdullah.tamemii@gmail.com>
2026-09-25 14:27:54 +0300

    Fix formatting for ZXing core dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,5 +47,5 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.6")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
-    implementation 'com.google.zxing:core:3.5.3'
+    implementation("com.google.zxing:core:3.5.3")
 }
