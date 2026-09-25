fbb7b8e79eb49409466355c97f04dd8fc9d1f1a8
abdullah14120 <abdullah.tamemii@gmail.com>
2026-09-25 14:25:52 +0300

    Move QRCodeGenerator to utils package

--- app/src/main/java/com/abdullah/wifibridge/utils/QRCodeGenerator.kt
+++ app/src/main/java/com/abdullah/wifibridge/utils/QRCodeGenerator.kt
@@ -1,4 +1,4 @@
-package com.abdullah.wifibridge.ui
+package com.abdullah.wifibridge.utils
 
 import android.graphics.Bitmap
 import android.graphics.Color
@@ -9,27 +9,22 @@ import com.google.zxing.qrcode.QRCodeWriter
 object QRCodeGenerator {
 
     /**
-     * يقوم بتوليد نص خاص للاتصال بشبكة الواي فاي حسب المعايير القياسية
+     * توليد نص مطبق لمعايير الاتصال بالواي فاي القياسية (WIFI:S:ssid;T:WPA;P:password;;)
      */
     fun generateWifiQrCode(ssid: String, password: String, securityType: String = "WPA", size: Int = 512): Bitmap? {
-        // نص التنسيق القياسي للواي فاي
         val qrContent = if (password.isEmpty()) {
             "WIFI:S:$ssid;T:nopass;;"
         } else {
             "WIFI:S:$ssid;T:$securityType;P:$password;;"
         }
 
-        return generateBitmap(qrContent, size)
-    }
-
-    private fun generateBitmap(content: String, size: Int): Bitmap? {
         return try {
             val hints = HashMap<EncodeHintType, Any>()
             hints[EncodeHintType.CHARACTER_SET] = "UTF-8"
-            hints[EncodeHintType.MARGIN] = 1 // تقليل الحواف الميتة حول الكود
+            hints[EncodeHintType.MARGIN] = 1
 
             val writer = QRCodeWriter()
-            val bitMatrix = writer.encode(content, BarcodeFormat.QR_CODE, size, size, hints)
+            val bitMatrix = writer.encode(qrContent, BarcodeFormat.QR_CODE, size, size, hints)
 
             val width = bitMatrix.width
             val height = bitMatrix.height
@@ -37,7 +32,6 @@ object QRCodeGenerator {
 
             for (x in 0 until width) {
                 for (y in 0 until height) {
-                    // لون الكود أسود والخلفية بيضاء لضمان سهولة القراءة عبر الكاميرا
                     bitmap.setPixel(x, y, if (bitMatrix.get(x, y)) Color.BLACK else Color.WHITE)
                 }
             }
