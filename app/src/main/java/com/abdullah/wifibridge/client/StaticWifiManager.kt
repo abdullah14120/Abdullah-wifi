523b341eff541b4714427c77664a05440300d66d
abdullah14120 <abdullah.tamemii@gmail.com>
2026-09-25 13:30:38 +0300

    Update StaticWifiManager.kt

--- app/src/main/java/com/abdullah/wifibridge/client/StaticWifiManager.kt
+++ app/src/main/java/com/abdullah/wifibridge/client/StaticWifiManager.kt
@@ -1,54 +1,49 @@
 package com.abdullah.wifibridge.client
 
 import android.content.Context
-import android.net.IpConfiguration
+import android.net.ConnectivityManager
 import android.net.LinkAddress
+import android.net.Network
+import android.net.NetworkRequest
 import android.net.StaticIpConfiguration
 import android.net.wifi.WifiNetworkSpecifier
-import android.net.NetworkRequest
-import android.net.ConnectivityManager
-import android.net.Network
 import android.os.Build
 import androidx.annotation.RequiresApi
 import java.net.InetAddress
 
 class StaticWifiManager(private val context: Context) {
 
-    // تطبيق ضبط الـ Static IP الحقيقي بدون VPN
+    @RequiresApi(Build.VERSION_CODES.S)
     fun createStaticIpConfiguration(customIp: String, gatewayIp: String): StaticIpConfiguration {
-        val staticIpConfig = StaticIpConfiguration()
-        
         val ipInet = InetAddress.getByName(customIp)
         val gatewayInet = InetAddress.getByName(gatewayIp)
 
-        // تعيين IP جهاز العميل وقناع الشبكة Prefix Length 24 (/255.255.255.0)
-        staticIpConfig.ipAddress = LinkAddress(ipInet, 24)
-        staticIpConfig.gateway = gatewayInet
+        val linkAddress = LinkAddress(ipInet, 24)
 
-        // تعيين الـ Gateway والـ DNS المخصص
-        staticIpConfig.dnsServers.add(gatewayInet)
-        staticIpConfig.dnsServers.add(InetAddress.getByName("8.8.8.8"))
-
-        return staticIpConfig
+        // استخدام Builder لتجنب مشكلة Package-Private Constructor
+        return StaticIpConfiguration.Builder()
+            .setIpAddress(linkAddress)
+            .setGateway(gatewayInet)
+            .setDnsServers(listOf(gatewayInet, InetAddress.getByName("8.8.8.8")))
+            .build()
     }
 
     @RequiresApi(Build.VERSION_CODES.Q)
-    fun connectToBridgeNetwork(ssid: String, passphrase: String, clientIp: String, gatewayIp: String) {
+    fun connectToBridgeNetwork(ssid: String, passphrase: String) {
         val specifier = WifiNetworkSpecifier.Builder()
             .setSsid(ssid)
             .setWpa2Passphrase(passphrase)
             .build()
 
         val request = NetworkRequest.Builder()
-            .addTransportType(android.net.NetworkCapabilities.TRANSPORT_WIFI) // تحديد صريح لواي فاي حقيقي
+            .addTransportType(android.net.NetworkCapabilities.TRANSPORT_WIFI)
             .setNetworkSpecifier(specifier)
             .build()
 
         val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        
+
         connectivityManager.requestNetwork(request, object : ConnectivityManager.NetworkCallback() {
             override fun onAvailable(network: Network) {
-                // تم الاتصال بنجاح بشبكة الواي فاي الحقيقية!
                 connectivityManager.bindProcessToNetwork(network)
             }
         })
